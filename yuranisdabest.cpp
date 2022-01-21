ab796ceb3b9165ecf0c62873ec25724e85b34db3
NTTT <tuyenntt.l.2023@gmail.com>
2022-01-21 14:56:54 +0700

    as

--- yuranisdabest.cpp
+++ yuranisdabest.cpp
@@ -28,6 +28,6 @@ int main()
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     /* freopen(task".inp" , "r" , stdin);
     freopen(task".out" , "w" , stdout);*/
-    
+    cout << 123;
     return 0;
 }
