c409412b46e884a24a0661f87b5ace0238bd2659
NTTT <tuyenntt.l.2023@gmail.com>
2022-01-21 14:49:17 +0700

    edit

--- yuranisdabest.cpp
+++ yuranisdabest.cpp
@@ -26,8 +26,5 @@ template <typename T1, typename T2> bool maximize(T1 &a, T2 b)
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    /* freopen(task".inp" , "r" , stdin);
-    freopen(task".out" , "w" , stdout);*/
-    
     return 0;
 }
